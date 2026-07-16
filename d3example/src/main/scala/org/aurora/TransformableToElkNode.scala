7c3c464caf0c0b75af068d95bdfd8bd94e05529d
yash-a-18 <yashamethiya2001@gmail.com>
2026-07-16 12:55:05 -0400

    remove duplicate edges by keeping edges just in the root node

--- d3example/src/main/scala/org/aurora/TransformableToElkNode.scala
+++ d3example/src/main/scala/org/aurora/TransformableToElkNode.scala
@@ -6,24 +6,21 @@ import typings.elkjs.libElkApiMod.ElkNode
 import scala.scalajs.js
 import scala.scalajs.js.JSConverters.*
 import org.aurora.AuroraElkUtils.getDrawableDescendants
-import org.aurora.AuroraElkUtils.getDrawableEdges
 
 trait TransformableToElkNode[A]:
     extension (a: A) def transformToElkNode: ElkNode
 
 given TransformableToElkNode[IssueCoordinate] with
   extension (ic: IssueCoordinate) def transformToElkNode: ElkNode =
-    ElkNode(id=ic.name + "%%Reference").setChildren(getDrawableDescendants(ic).toJSArray)
-                       .setEdges(getDrawableEdges(ic).toJSArray)
+    ElkNode(id=ic.name + "%%Reference")
+      .setChildren(getDrawableDescendants(ic).toJSArray)
 
 given TransformableToElkNode[OrderCoordinate] with
   extension (oc: OrderCoordinate) def transformToElkNode: ElkNode =
-    ElkNode(id=oc.name + "%%Coordinate").setChildren(getDrawableDescendants(oc).toJSArray)
-                       .setEdges(getDrawableEdges(oc).toJSArray)
+    ElkNode(id=oc.name + "%%Coordinate")
+      .setChildren(getDrawableDescendants(oc).toJSArray)
 
 given TransformableToElkNode[NL_STATEMENT] with
   extension (n: NL_STATEMENT) def transformToElkNode: ElkNode =
-    ElkNode(id=n.name + "%%Statement").setChildren(getDrawableDescendants(n).toJSArray)
-                      .setEdges(getDrawableEdges(n).toJSArray)
-
-
+    ElkNode(id=n.name + "%%Statement")
+      .setChildren(getDrawableDescendants(n).toJSArray)
