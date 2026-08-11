f092ea7fe79d305279c2511e9a5884d5854b31ba
saki-aurora <skaushi1@lakeheadu.ca>
2026-08-11 07:31:59 -0400

    test(meet): add file-based Aurora meet scenarios

--- pcmalgebra/src/test/scala/org/aurora/sjsast/BaseAsyncTest.scala
+++ pcmalgebra/src/test/scala/org/aurora/sjsast/BaseAsyncTest.scala
@@ -1,64 +1,68 @@
 package org.aurora.sjsast
 
-
 import org.scalatest._
 
 import wordspec._
 import matchers._
 import scala.concurrent.Future
-import org.aurora.utils.fileutils.createFileIfNotExists
 import scala.concurrent.ExecutionContext
 import scala.scalajs.js
-export org.aurora.utils.{fileutils,fs}
+export org.aurora.utils.{fileutils, fs}
 
-class BaseAsyncTest extends wordspec.AsyncWordSpec with should.Matchers{
+class BaseAsyncTest extends wordspec.AsyncWordSpec with should.Matchers {
   export JoinMeet.given
   // export cats.syntax.semigroup._ // for |+|
 
   export scala.concurrent.Future
   // export cats.Show
-  // export cats.syntax.show._ 
+  // export cats.syntax.show._
   // export Show.given
-  export scala.scalajs.js.JSConverters._ 
+  export scala.scalajs.js.JSConverters._
 
   import scala.scalajs.concurrent.JSExecutionContext.Implicits.queue
   override implicit def executionContext: ExecutionContext = queue
 
   private lazy val testResourcesPath = fileutils.testResourcesPath
-  private lazy val basefilename = this.getClass.getSimpleName.replace("Test","")
-  private lazy val fullyQualifiedName = this.getClass.getName.replace("Test","").replace(".",fileutils.separator)
+  private lazy val basefilename = this.getClass.getSimpleName.replace("Test", "")
+  private lazy val fullyQualifiedName = this.getClass.getName.replace("Test", "").replace(".", fileutils.separator)
   private lazy val testPath = s"$testResourcesPath${fileutils.separator}$fullyQualifiedName"
 
+  // info() wrapped in a future
+  protected def finfo(output: String) = Future(info(s"$output"))
 
-  //info() wrapped in a future
-  protected def finfo(output:String) =  Future(info(s"$output"))
-
-  def testfilepath(index:Int) = 
-    val path =  s"$testResourcesPath${fileutils.separator}$fullyQualifiedName-$index.aurora"
-    createFileIfNotExists(path)
+  def testfilepath(name: String) =
+    val path = s"$testPath-$name.aurora"
+    if !fs.existsSync(path) then fail(s"missing test fixture: $path")
     path
 
-  def ftestfilepath(index:Int) = Future(testfilepath(index))  
+  def testfilepath(index: Int): String = testfilepath(index.toString)
 
+  def ftestfilepath(index: Int) = Future(testfilepath(index))
 
-  def testfiletext(index:Int) = 
+  def testfiletext(index: Int) =
     val path = testfilepath(index)
-    createFileIfNotExists(path)
     fileutils.readFileSync(path)
 
-  def ftestfiletext(index:Int) = Future(testfiletext(index))  
+  def ftestfiletext(index: Int) = Future(testfiletext(index))
 
-  def parse(index:Int) = fileutils.parse(testfilepath(index)).toFuture.recover(
+  def parse(name: String): Future[GenAst.PCM] = fileutils
+    .parse(testfilepath(name))
+    .toFuture
+    .recover(
       {
-        case _: js.JavaScriptException => 
+        case _: js.JavaScriptException =>
           // Handle JavaScript parsing errors
           fail("Parse failed with JavaScript error")
-        case ex: Exception => 
+        case ex: Exception =>
           // Handle other exceptions
           fail(s"Parse failed: ${ex.getMessage}")
       }
     )
 
-  def ir(index:Int) = parse(index).map(PCM(_)) 
+  def parse(index: Int): Future[GenAst.PCM] = parse(index.toString)
+
+  def ir(name: String): Future[PCM] = parse(name).map(PCM(_))
+
+  def ir(index: Int): Future[PCM] = ir(index.toString)
 
 }
